5bdb7d185b5dfaf34e509eb1698dcc142c0133b7
Choi JaeHoon <jhchoi09@naver.com>
2023-04-27 16:10:04 +0900

    Rechecking Development Env

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import torch
 
 DEVICE = torch.cuda.is_available()
 print(f'{DEVICE}, {torch.__version__}')
+print(f'{torch.cuda.current_device()}')
+print(f'{torch.cuda.device_count()}')
+print(f'{torch.cuda.get_device_name(0)}')
